b71fac45707cbddd981d32dfb72d0a498b1b7d48
hollylamb <39955692+hollylamb@users.noreply.github.com>
2018-07-03 15:14:05 +0100

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -3,4 +3,21 @@
 def letterCapitalise(str):
  return (str.title())
 
-print letterCapitalise(input())
+print (letterCapitalise(input()))
+
+
+#simplesymbolschallenge
+
+def SimpleSymbols(str): 
+  
+  str = '=' + str + '='
+
+  for i in range(0, len(str)):
+     
+    if str[i].isalpha():
+
+  
+      if str[i-1] != '+' or str[i+1] != '+':
+        return False
+
+  return True
